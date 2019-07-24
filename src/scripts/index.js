4b6aade7f1c58d0f8b4ba10bb4b14995a6f82378
Sam Walker <jack.nasal@gmail.com>
2019-07-24 11:27:29 -0400

    added webpack and npm stuff

---
+++ src/scripts/index.js
@@ -0,0 +1,17 @@
+/* global shoppingList, store, api */
+
+import store from './store.js';
+import api from './api.js';
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
+
